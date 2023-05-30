fedb0cfd1af7a311b02bdca95bef1865dd0ecb13
shanto <shantoa729@gmail.com>
2023-05-31 01:04:09 +0900

    next1

--- say.py
+++ say.py
@@ -1,6 +1,8 @@
 import cowsay
 import sys
+from sayings import hello
 
 if len(sys.argv) == 2:
     cowsay.cow("Hello, " + sys.argv[1])
     cowsay.trex("Hello, " + sys.argv[1])
+    hello(sys.argv[1])
