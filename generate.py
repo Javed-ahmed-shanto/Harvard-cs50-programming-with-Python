4fd4ffb9a6ecd6d8215be40ae17f025662e9c4a9
shanto <shantoa729@gmail.com>
2023-05-31 00:02:00 +0900

    random

---
+++ generate.py
@@ -0,0 +1,11 @@
+import random
+
+cards = ["Jack", "King", "Queen", "Joker"]
+
+random.shuffle(cards)
+for card in cards: 
+    print(card)
+
+#random.choice([])
+#random.shuffle
+#random.randint(a,b)
