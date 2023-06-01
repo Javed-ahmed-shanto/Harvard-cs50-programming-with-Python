3a079511a8276df0e3abe2cf119b32ab54230ed4
shanto <shantoa729@gmail.com>
2023-06-01 17:48:33 +0900

    students.py updated

--- students.py
+++ students.py
@@ -1,4 +1,18 @@
+# with open("students.csv") as file:
+#     for line in file:
+#         name, house = line.rstrip().title().split(",") # as split sperates two things in here, we are sepearating in first[0] as name and second[1] as houses
+#         print(f"{name} is in{house}")
+
+students = []
+
 with open("students.csv") as file:
     for line in file:
-        row = line.rstrip().split(",")
-        print(f"{row[0]} is in {row[1]}")
+        name, house = line.title().rstrip().split(",")
+        student = {"name": name, "house": house}
+        students.append(student)
+
+# def get_name(student):
+#     return student["name"]
+
+for student in sorted(students, key = lambda student: student["name"]): # Sorted using studnets names
+    print(f"{student['name']} is in{student['house']}")
