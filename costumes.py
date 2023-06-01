6092f864a2419fc82bf3b72d5ef33e449d8d7469
shanto <shantoa729@gmail.com>
2023-06-01 21:00:36 +0900

    a lot has been going on ^_^

---
+++ costumes.py
@@ -0,0 +1,5 @@
+import sys
+
+from PIL import Image
+images = []
+
--- students.py
+++ students.py
@@ -1,18 +1,51 @@
+# Houses:
+# Shanto, All
+# harry, Gryffindor
+# ron, Gryffindor
+# Draco Malfoy, Slytherin
+
 # with open("students.csv") as file:
 #     for line in file:
 #         name, house = line.rstrip().title().split(",") # as split sperates two things in here, we are sepearating in first[0] as name and second[1] as houses
 #         print(f"{name} is in{house}")
 
-students = []
 
-with open("students.csv") as file:
-    for line in file:
-        name, house = line.title().rstrip().split(",")
-        student = {"name": name, "house": house}
-        students.append(student)
+# with open("students.csv") as file:
+#     for line in file:
+#         name, house = line.title().rstrip().split(",")
+#         student = {"name": name, "house": house}
+#         students.append(student)
 
 # def get_name(student):
 #     return student["name"]
 
-for student in sorted(students, key = lambda student: student["name"]): # Sorted using studnets names
-    print(f"{student['name']} is in{student['house']}")
+# import csv
+
+# students = []
+
+# with open("students.csv") as file:
+#     reader = csv.DictReader(file)
+#     for row in reader:
+#         students.append({"name": row["name"], "home": ["home"]})/ or just (row)
+
+# for student in sorted(students, key=lambda student: student["name"]): # Sorted using studnets names
+#     print(f"{student['name']} is from {student['home']}")
+
+
+# import csv
+
+# name = input("What's your name? ")
+# home = input("Where's your home? ")
+
+# with open("students.csv", "a") as file:
+#     writer = csv.writer(file)
+#     writer.writerow([name, home])
+
+import csv
+
+name = input("What's your name? ")
+home = input("Where's your home? ")
+
+with open("students.csv", "a") as file:
+    writer = csv.DictWriter(file, fieldnames=["name", "home"])
+    writer.writerow({"name": name, "home": home})
