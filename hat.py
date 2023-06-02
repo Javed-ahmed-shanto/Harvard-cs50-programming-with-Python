ef3df8bd9273119abb3069b33bd7ad6f3deaddbe
shanto <shantoa729@gmail.com>
2023-06-03 01:24:30 +0900

    initiates hat of harry potter

---
+++ hat.py
@@ -0,0 +1,14 @@
+import random
+
+class Hat:
+    def __init__(self):
+        self.houses = ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]
+
+    def sort(self, name):
+        print(name, "is in", random.choice(self.houses))
+
+hat = Hat()
+hat.sort("Harry")
+
+# if __name__ == "__main__":
+#     main()
