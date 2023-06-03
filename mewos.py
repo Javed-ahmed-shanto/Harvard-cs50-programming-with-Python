e377a563aa00d908ee68c2376b41918df73fd9a6
shanto <shantoa729@gmail.com>
2023-06-03 22:27:00 +0900

    init unpack

--- mewos.py
+++ mewos.py
@@ -16,15 +16,25 @@
 # number: int = int(input("Number: "))
 # meow(number)
 
-def meow(n: int) -> str:  # Typehints
-    """Meow n times. formal pythonic way"""
-    """ 
-    or like this.
-    :type n: int
-    :rtype: str
-    """
-    return "meow\n" * n
+# def meow(n: int) -> str:  # Typehints
+#     """Meow n times. formal pythonic way"""
+#     """ 
+#     or like this.
+#     :type n: int
+#     :rtype: str
+#     """
+#     return "meow\n" * n
 
-number: int = int(input("Number: "))
-meows: str = meow(number)
-print(meows, end="")
+# number: int = int(input("Number: "))
+# meows: str = meow(number)
+# print(meows, end="")
+
+import argparse
+
+parser = argparse.ArgumentParser(description="Meow like a cat")
+parser.add_argument("-n", default=1, help ="number of times to meow", type=int)
+args = parser.parse_args()
+
+
+for _ in range(int(args.n)):
+    print("meow")
---
+++ unpack.py
@@ -0,0 +1,14 @@
+# def total(galleons, sickles, knuts):
+#     return (galleons * 17 + sickles) * 29 + knuts
+
+# coins = [100, 50, 25]
+
+# print(total(*coins), "Knuts") """ * usage for unpacking"""
+
+# def total(galleons, sickles, knuts):
+#     return (galleons * 17 + sickles) * 29 + knuts
+
+# coins = {"galleons": 100, "sickles": 50, "knuts": 25}
+
+# print(total(**coins), "Knuts")  # ** used for dictionary unpacking
+
